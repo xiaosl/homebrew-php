82bfe6f6bf5ee0f86ba7ad37619900d08970703c
xiaosl <xiaosl@126.com>
2015-10-10 23:50:50 +0800

    rm bottle

--- Formula/php56-stomp.rb
+++ Formula/php56-stomp.rb
@@ -7,14 +7,6 @@ class Php55Stomp < AbstractPhp55Extension
   sha256 "9cf4180a816bac08300c45083410ca536200bd4940db0174026b9a825161f159"
   head "https://github.com/JosephMoniz/php-stomp.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    revision 1
-    sha256 "44ffc652615b0d5279a4a6b4a17c890efc41f2150b1d718b2dfac38c826ee417" => :el_capitan
-    sha256 "3d41384e4d5072f3dcc56127f298fff32b52f4167fd379d618003c5474e84634" => :yosemite
-    sha256 "a1e78657ee4a318092a8af298ef63ac511dc2db38a85b3e13ebfeffb5c968aac" => :mavericks
-  end
-
   def install
     ENV.universal_binary if build.universal?
 
